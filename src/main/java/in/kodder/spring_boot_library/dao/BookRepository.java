1009a03dba7cb608b1d8cdfbf122e01171a0317c
Avinash Kumar <akgpcamp@gmail.com>
2024-06-02 19:02:53 +0530

    application properties file

--- src/main/java/in/kodder/spring_boot_library/dao/BookRepository.java
+++ src/main/java/in/kodder/spring_boot_library/dao/BookRepository.java
@@ -1,7 +1,12 @@
 package in.kodder.spring_boot_library.dao;
 
 import in.kodder.spring_boot_library.entity.Book;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.data.domain.Pageable;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
